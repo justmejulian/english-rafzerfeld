9236ea8cfd243c2fee728df8a4dcec77ce425768
Julian Visser <julianvisser@Julians-MacBook-Pro.local>
2020-11-08 15:32:23 +0100

    added redirect to /en

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,14 @@ const { createFilePath } = require("gatsby-source-filesystem");
 const { fmImagesToRelative } = require("gatsby-remark-relative-images");
 
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+  const { createPage, createRedirect } = actions;
+
+  createRedirect({
+    fromPath: `/`,
+    toPath: `/en`,
+    isPermanent: `true`,
+    force: `true`,
+  });
 
   return graphql(`
     {
