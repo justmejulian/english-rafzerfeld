c29f8eafc41fe0fd1c2ff6458a57e5affcc840e8
Julian Visser <chulio.visser@gmail.com>
2021-01-17 16:16:00 +0100

    removed unused styling

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -2,31 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql, StaticQuery } from "gatsby";
 
+import styles from "./BlogRoll.module.css";
+
 class BlogRoll extends React.Component {
   render() {
     const { data } = this.props;
     const { edges: posts } = data.allMarkdownRemark;
 
     return (
-      <div className='columns is-multiline'>
+      <div >
         {posts &&
           posts.map(({ node: post }) => (
-            <div className='is-parent column is-6' key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? "is-featured" : ""
-                }`}
-              >
+            <div key={post.id}>
+              <article>
                 <header>
-                  <p className='post-meta'>
+                  <p >
                     <Link
-                      className='title has-text-primary is-size-4'
                       to={post.fields.slug}
                     >
                       {post.frontmatter.title}
                     </Link>
                     <span> &bull; </span>
-                    <span className='subtitle is-size-5 is-block'>
+                    <span >
                       {post.frontmatter.date}
                     </span>
                   </p>
@@ -35,7 +32,7 @@ class BlogRoll extends React.Component {
                   {post.excerpt}
                   <br />
                   <br />
-                  <Link className='button' to={post.fields.slug}>
+                  <Link to={post.fields.slug}>
                     Keep Reading →
                   </Link>
                 </p>
