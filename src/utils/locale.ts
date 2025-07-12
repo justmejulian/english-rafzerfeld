401d0f8dbca967f32ae4d83df0b5444e7d8e9083
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-07-12 20:40:21 +0200

    Format

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,12 +1,12 @@
-export type Locale = "en" | "de";
+export type Locale = 'en' | 'de';
 
 // todo: store in localStorage
 
 export function getLocaleFromURL(url: URL): Locale {
-  const locale = url.pathname.split("/")[1];
+  const locale = url.pathname.split('/')[1];
 
-  if (!locale || (locale !== "en" && locale !== "de")) {
-    throw new Error("Invalid locale in URL");
+  if (!locale || (locale !== 'en' && locale !== 'de')) {
+    throw new Error('Invalid locale in URL');
   }
   return locale;
 }
