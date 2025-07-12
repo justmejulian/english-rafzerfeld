5bf92e0d5819ad51122d16bb7e1e1aaf47518d54
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-07-12 20:40:21 +0200

    Added preactjs and Button example

---
+++ src/components/preact/Button.tsx
@@ -0,0 +1,11 @@
+interface ButtonProps {
+  title: string;
+}
+
+function Button({ title }: ButtonProps) {
+  return (
+    <button onClick={() => console.log('Button clicked!')}>{title}</button>
+  );
+}
+
+export default Button;
