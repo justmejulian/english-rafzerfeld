81fde1e8351ba585436e9ce6440deddd0ce4b4cf
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-07-12 20:40:21 +0200

    Added tailwindcss and used it in button

--- src/components/preact/Button.tsx
+++ src/components/preact/Button.tsx
@@ -4,7 +4,12 @@ interface ButtonProps {
 
 function Button({ title }: ButtonProps) {
   return (
-    <button onClick={() => console.log('Button clicked!')}>{title}</button>
+    <button
+      className="inline-block rounded-sm border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:ring-3 focus:outline-hidden"
+      onClick={() => console.log('Button clicked')}
+    >
+      {title}
+    </button>
   );
 }
 
