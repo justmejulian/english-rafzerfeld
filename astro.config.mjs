2189639f4382a8be5fe6f579d1b53e60bc091f9d
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-08-18 16:20:44 +0200

    add site/base for correct routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://justmejulian.ch',
+  base: 'english-rafzerfeld',
   integrations: [preact()],
 
   i18n: {
