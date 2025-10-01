85d1ace14b5ea5e44e3934d907c230bfb89adfd4
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-10-01 10:56:05 +0200

    set new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://english.justmejulian.ch',
+  site: 'https://www.english-rafzerfeld.ch',
   integrations: [preact()],
 
   i18n: {
